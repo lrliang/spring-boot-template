29a3f5c3ffda93c4718297a46db747daf3afe19c
lingrui <lingrui.liang@capgemini.com>
2022-04-03 12:44:26 +0800

    init

--- src/main/java/com/jtp/hr/Application.java
+++ src/main/java/com/jtp/hr/Application.java
@@ -2,6 +2,7 @@ package com.jtp.hr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -9,6 +10,7 @@ import java.util.TimeZone;
 import static java.time.ZoneId.of;
 import static java.util.TimeZone.getTimeZone;
 
+@EnableWebMvc
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jtp/hr/common/configuration/SwaggerConfiguration.java
+++ src/main/java/com/jtp/hr/common/configuration/SwaggerConfiguration.java
@@ -1,12 +1,12 @@
-package com.jtp.hr.common.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
-@Profile(value = {"local", "dev"})
-public class SwaggerConfiguration {
-
-}
+//package com.jtp.hr.common.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//@Profile(value = {"local", "dev"})
+//public class SwaggerConfiguration {
+//
+//}
